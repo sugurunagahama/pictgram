a0f95093081ca6d24c652893a8b612767d6844d3
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-03-26 10:10:52 +0000

    Rails07

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'topics/new'
+
   get 'sessions/new'
 
   root 'pages#index'
   get 'pages/link'
 
-  resources :users
-  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  
 end
