a6bde0f1401572468a740294ab0fe9f7b3b94d31
suguru <ec2-user@ip-172-31-18-186.us-east-2.compute.internal>
2021-03-28 08:38:26 +0000

    05

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    
   end
 
-  def create
-    user = User.find_by(user_params)
+ def create
+    user = User.find_by(email: user_params[:email])
     
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(user_params[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
-  end
+ end
   
    def destroy
     log_out
     redirect_to root_url, info: 'ログアウトしました'
    end
-
+   
   private
   def log_in(user)
     session[:user_id] = user.id
@@ -27,6 +28,10 @@ class SessionsController < ApplicationController
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def user_params
+    params.require(:session).permit(:email, :password)
   end
 
 end
